33c4cca4886d9157e987488f7535763eaf6d675f
Yatamoku <yatamoku@gmail.com>
2016-01-21 19:26:53 +0000

    Update yesiknowthisisonthewiki.lua

--- lua/autorun/server/yesiknowthisisonthewiki.lua
+++ lua/autorun/server/yesiknowthisisonthewiki.lua
@@ -1,9 +1,5 @@
 hook.Add("PlayerAuthed", "PlayerAuthed", function ()
-	
 	for k,v in pairs(player.GetAll()) do
-
 		PrintMessage(HUD_PRINTTALK, v:Name().." has successfully connected. ("..v:SteamID()..").")
-
 	end
-
 end)
